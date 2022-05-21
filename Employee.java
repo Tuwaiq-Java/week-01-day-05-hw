1c64f1ed5452b05533f267b0ed5953727c4620a6
Lina Aloqaily <ogililina@gmail.com>
2022-05-21 15:12:57 +0300

    project

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public abstract class Employee implements PayAble {
+    private String id, name;
+
+    public Employee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee {" + "id = " + id  + ", name = " + name + '}';
+    }
+}
